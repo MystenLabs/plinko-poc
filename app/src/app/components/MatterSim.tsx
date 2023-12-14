a0c2477bcc11b5a8a8ae0ce1f0e7b4c61bf623ec
geomel <george.melas@mystenlabs.com>
2023-12-14 10:40:25 +0200

    Initial implementation with vector

--- app/src/app/components/MatterSim.tsx
+++ app/src/app/components/MatterSim.tsx
@@ -1,53 +1,62 @@
 import React, { useEffect } from 'react';
-import Matter, { Engine, Render, Runner, Bodies, Composite, Vector } from 'matter-js';
+import Matter, { Engine, Render, Runner, Bodies, Composite, Vector, IPair } from 'matter-js';
 
 const MatterSim: React.FC = () => {
+  // Create a physics engine instance
   const engine = Engine.create();
-  const ballSize = 7;
-  const ballElasticity = 0.1;
-  const ballSpeed = 3;
-  const ballCategory = 0x0001;
 
-  // [0,1,0,1,0,1,0,1,0,1,0,1]
+  // Ball properties
+  const ballSize = 7;
+  const ballElasticity = 0.1; // Bounciness of the ball
+  const ballSpeed = 4;
+  const ballCategory = 0x0001; // Collision category for the ball
+
+  // Vector to control the ball direction after each collision
+  const directionVector = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+  let vectorIndex = 0; // Current index in the direction vector
 
   useEffect(() => {
     console.log('MatterSim component is rendering.');
 
+    // Define the dimensions of the simulation world
     const worldWidth = 800;
-    const startPins = 3;
-    const pinLines = 12;
-    const pinSize = 5;
-    const pinGap = 40;
-    const desiredGapIndex = 2; // Index of the desired gap (0-based)
-    const gapWidth = pinGap;
-    const desiredBoxX = -250;//(desiredGapIndex + 0.5) * gapWidth; // X-coordinate of the center of the desired box
+    const startPins = 3; // Starting number of pins in the first line
+    const pinLines = 12; // Total number of lines of pins
+    const pinSize = 5; // Size of each pin
+    const pinGap = 40; // Gap between pins
 
+    // Get the DOM element to render the simulation
     const container = document.getElementById('matter-canvas-container');
 
+    // Create a renderer for the physics engine
     const render = Render.create({
-      element: container,
-      engine: engine,
+      element: container, // DOM element to render within
+      engine: engine, // Physics engine to use
       options: {
         width: 800,
         height: 600,
         background: 'white',
-        showVelocity: true,
-        showAngleIndicator: true,
+        showVelocity: true, // Show velocity vectors in the simulation
+        showAngleIndicator: true, // Show angle indicators in the simulation
       },
     });
 
-    const pins: Matter.Body[] = [];
+    // Array to store all pin bodies
+    const pins = [];
+
+    // Create a ball body
     const ball = Bodies.circle(362, 0, ballSize, {
-      restitution: ballElasticity,
+      restitution: ballElasticity, // Bounciness
       friction: 1,
       density: 0.1,
       frictionStatic: 0,
       render: {
-        fillStyle: 'blue',
+        fillStyle: 'blue', // Color of the ball
       },
     });
-    ball.collisionFilter.category = ballCategory;
+    ball.collisionFilter.category = ballCategory; // Set collision category
 
+    // Loop to create pins in a triangular formation
     for (let l = 0; l < pinLines; l++) {
       const linePins = startPins + l;
       const lineWidth = linePins * pinGap;
@@ -55,65 +64,61 @@ const MatterSim: React.FC = () => {
         const pinX = worldWidth / 2 - lineWidth / 2 + i * pinGap;
         const pinY = 100 + l * pinGap;
         const pin = Bodies.circle(pinX, pinY, pinSize, {
-          isStatic: true,
+          isStatic: true, // Pins are stationary
           collisionFilter: {
-            group: -1,
+            group: -1, // Negative collision group for intra-pin collisions
           },
         });
         pins.push(pin);
 
+        // Create buckets at the bottom of the pin setup
         if (l === pinLines - 1 && i < linePins - 1) {
           const bucketX = pinX + pinGap / 2;
           const bucketY = pinY + pinGap / 2;
           const bucket = Bodies.rectangle(bucketX, bucketY, pinGap, pinGap, {
-            isStatic: true,
+            isStatic: true, // Buckets are stationary
           });
-
           Composite.add(engine.world, bucket);
         }
       }
     }
 
-    // function findDesiredBoxX(boxIndex: number) {
-
-
-    // };
-
-    console.log(pins.length);
-    console.log(pins[88]);
-    console.log(desiredBoxX);
-
+    // Add all pins to the world
     Composite.add(engine.world, pins);
+    // Add the ball to the world
     Composite.add(engine.world, [ball]);
 
-    Matter.Events.on(engine, 'collisionStart', (event) => {
-      event.pairs.forEach((pair) => {
+    // Set up a collision event listener
+    Matter.Events.on(engine, 'collisionStart', (event: any) => {
+       console.log("Collision detected");
+      event.pairs.forEach((pair: any) => {
+        // Check if the collision involves the ball
         if (pair.bodyA === ball || pair.bodyB === ball) {
-          const pin = pair.bodyA === ball ? pair.bodyB : pair.bodyA;
-          const pinX = pin.position.x;
-          const ballX = ball.position.x;
+          // Determine the direction based on the vector
+          const direction = directionVector[vectorIndex] === 1 ? 1 : -1;
+          // Update the index for the next collision
+          vectorIndex = (vectorIndex + 1) % directionVector.length;
+          // Calculate the force magnitude based on the ball speed
+          const forceMagnitude = ballSpeed * direction;
+          // Create a velocity vector based on the force magnitude
+          const velocity = Vector.create(forceMagnitude, ball.velocity.y);
+          // Apply the velocity to the ball
 
-          // Calculate the direction based on the ball's position relative to the pin
-          const direction = ballX > desiredBoxX ? 1 : -1;
+          // Matter.Body.setVelocity(ball, velocity);
+        
+          Matter.Body.setVelocity(ball, { x: 5, y: 0 });
 
-          engine.gravity = {x:0.1, y:0.1, scale: 0.001}; // prepei na kano kati me to gravity sto x gia na kano to auto me to desiredPath isos
-          // kai meta na to setato sto kanoniko pali gia na min fainetai oti to pirazo?
-
-          // Calculate the forceMagnitude based on your desired speed
-          const forceMagnitude = 0.0037 * (desiredBoxX - ballX);
-
-          // Set the velocity to move the ball towards the desired box
-          const velocity = Vector.create(direction * forceMagnitude, ball.velocity.y);
-          Matter.Body.setVelocity(ball, velocity);
         }
       });
     });
 
+    // Run the renderer
     Render.run(render);
-
+    // Create a runner to run the engine
     const runner = Runner.create();
     Runner.run(runner, engine);
 
+    // Cleanup function to stop the renderer and runner when the component unmounts
     return () => {
       Render.stop(render);
       Runner.stop(runner);
