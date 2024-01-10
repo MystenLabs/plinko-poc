bd8de748629d39c31c67761f31c3bd04f40a4e87
Nick Pappas <nick.pappas@Nicks-MBP.home>
2024-01-10 11:22:14 +0200

    Change reactStrictMode

--- app/next.config.js
+++ app/next.config.js
@@ -8,7 +8,7 @@ const withPWA = require("next-pwa")({
 });
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
