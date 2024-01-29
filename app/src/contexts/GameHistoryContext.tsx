dd8b9bf80b671aa00c3b343b5231d64185b45a28
Nick Pappas <nick.pappas.ee@gmail.com>
2024-01-29 17:15:25 +0200

    Keeping the history until player hit play again . Also fix the total won to counter the sui that player won

--- app/src/contexts/GameHistoryContext.tsx
+++ app/src/contexts/GameHistoryContext.tsx
@@ -1,14 +1,19 @@
 "use client";
 import React, { createContext, useContext, useState } from "react";
+import { set } from "zod";
 
 interface GameHistoryContextType {
   colors: string[]; // Only storing colors now
+  totalWon: number;
+  addTotalWon: (amount: number, lastColorIndex: number) => void;
   addColor: (color: string | undefined) => void;
   resetHistory: () => void;
 }
 
 const GameHistoryContext = createContext<GameHistoryContextType>({
   colors: [],
+  totalWon: 0,
+  addTotalWon: () => {},
   addColor: () => {},
   resetHistory: () => {},
 });
@@ -19,6 +24,8 @@ export const GameHistoryProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [colors, setColors] = useState<string[]>([]);
+  const [totalWon, setTotalWon] = useState<number>(-1);
+  const [lastColorIndexHistory, setLastColorIndexHistory] = useState<number>(0);
 
   const addColor = (color: string | undefined) => {
     if (color !== undefined) {
@@ -26,12 +33,23 @@ export const GameHistoryProvider: React.FC<{ children: React.ReactNode }> = ({
     }
   };
 
+  const addTotalWon = (amount: number, lastColorIndex: number) => {
+    if (lastColorIndexHistory !== lastColorIndex) {
+      setLastColorIndexHistory(lastColorIndex);
+      setTotalWon((prevTotalWon) => prevTotalWon + amount);
+    }
+  };
+
   const resetHistory = () => {
+    setLastColorIndexHistory(0);
     setColors([]);
+    setTotalWon(-1);
   };
 
   return (
-    <GameHistoryContext.Provider value={{ colors, addColor, resetHistory }}>
+    <GameHistoryContext.Provider
+      value={{ colors, addColor, resetHistory, totalWon, addTotalWon }}
+    >
       {children}
     </GameHistoryContext.Provider>
   );
