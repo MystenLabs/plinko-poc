3679b4741efbe650943369555abedecc50c8d092
Nick Pappas <nick.pappas.ee@gmail.com>
2024-02-07 17:07:40 +0200

    Remove extra things from the ui to make it simple to work on top of that

--- app/src/components/general/Paper.tsx
+++ app/src/components/general/Paper.tsx
@@ -8,7 +8,7 @@ interface PaperProps {
 export const Paper = ({ className = "", children }: PaperProps) => {
   return (
     <div
-      className={`min-w-[200px] max-w-full p-2 md:p-5 bg-opacity-0 shadow-xl rounded-xl ${className}`}
+      className={`min-w-[200px] max-w-full p-2 md:p-5 bg-opacity-0 shadow-xl rounded-xl ${className} `}
     >
       {children}
     </div>
