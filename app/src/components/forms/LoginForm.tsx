9d532620ce32112fd2027847c61d77555b5b8a37
geomel <george.melas@mystenlabs.com>
2024-02-27 21:02:22 +0200

    Add Plinko game introduction and Mysten Labs info with Google sign-in option

--- app/src/components/forms/LoginForm.tsx
+++ app/src/components/forms/LoginForm.tsx
@@ -50,21 +50,53 @@ export const LoginForm = () => {
   if (isAuthLoading || user.role !== USER_ROLES.ROLE_4) {
     return <Spinner />;
   }
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h3 className="font-semibold text-lg text-center mb-5"></h3>
-      {!!authURLs && (
-        <div className="flex flex-col md:flex-row items-center justify-center">
-          <Link
-            href={authURLs[USER_ROLES.ROLE_1]}
-            className="flex items-center space-x-2 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white w-full md:w-auto rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1"
-          >
-            <Image src="/google.svg" alt="Google" width={20} height={20} />
-            <div>Sign In To Play</div>
-          </Link>
+    <div className="flex flex-col items-center space-y-[20px]">
+      <div className="bg-white flex flex-col p-[60px] max-w-[480px] mx-auto rounded-[24px] items-center space-y-[60px]">
+        <Image
+          src="/general/mysten-logo-red.svg"
+          alt="Mysten Labs"
+          width={160}
+          height={20}
+        />
+        <div className="flex flex-col space-y-[30px] items-center">
+          <div className="flex flex-col space-y-[20px] items-center">
+            <div className="font-[700] text-[20px] text-center">
+              Plinko on Sui Blockchain <br /> 
+            </div>
+            <div className="text-center text-opacity-90 text-[14px] text-[#4F4F4F]">
+              Plinko is a game of chance where balls are dropped from 
+              the top of a pegged board, and the outcome is determined by the path they take. 
+              Our Sui blockchain implementation offers a unique experience by integrating cryptographic 
+              techniques to ensure fairness and transparency.
+             You can test the Plinko game on testnet.<p> Log in with you google account to play.</p>
+            </div>
+          </div>
+          {!!authURLs && (
+      <div className="flex flex-col md:flex-row items-center justify-center">
+        <Link
+          href={authURLs[USER_ROLES.ROLE_1]}
+          className="flex items-center space-x-2 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white w-full md:w-auto rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1"
+        >
+          <Image src="/google.svg" alt="Google" width={20} height={20} />
+          <div>Sign In To Play</div>
+        </Link>
+      </div>
+    )}
         </div>
-      )}
+      </div>
+      <div className="flex flex-col items-center text-white text-[12px]">
+        <div className="text-center">Learn more about Mysten Labs at</div>
+        <Link
+          href="https://mystenlabs.com"
+          target="_blank"
+          rel="noopenner noreferrer"
+          className="underline"
+        >
+          mystenlabs.com
+        </Link>
+      </div>
     </div>
   );
+
 };
