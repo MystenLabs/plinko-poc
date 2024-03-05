c0d9ab06238f7e4b5d4e11c881ace46dab3b34b1
Nick Pappas <nick.pappas.ee@gmail.com>
2024-03-05 13:29:03 +0200

    Removing gray line from pop up also fixing to allow the bet if it have the balance

--- app/src/components/InsufficientCoinBalance.tsx
+++ app/src/components/InsufficientCoinBalance.tsx
@@ -15,7 +15,7 @@ const PopupComponent2 = () => {
   return (
     <>
       {popupInsufficientCoinBalanceIsVisible && (
-        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center z-10 ">
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center z-20 ">
           <div
             className="p-5 border w-[600px] bg-white rounded-3xl shadow-lg "
             onClick={(e) => e.stopPropagation()}
@@ -29,7 +29,7 @@ const PopupComponent2 = () => {
                 <p>Sent SUI to your address</p>
                 <p>Or request SUI from the header</p>
               </div>
-              <div className="self-stretch bg-white border-t-2 justify-start items-center gap-2 inline-flex">
+              <div className="self-stretch bg-white  justify-start items-center gap-2 inline-flex">
                 <button
                   onClick={handleTryAgain}
                   className="grow shrink basis-0 self-stretch px-5 py-4 bg-black rounded-[38px] justify-center items-center gap-2 flex"
