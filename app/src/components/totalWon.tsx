3a17663e7ae1fc9619a96251cc93cf613e55dcaf
Nick Pappas <nick.pappas.ee@gmail.com>
2024-03-08 09:45:00 +0200

    Adding at Round earnings a counter for the last ball wons

--- app/src/components/totalWon.tsx
+++ app/src/components/totalWon.tsx
@@ -3,19 +3,28 @@ import { useGameHistory } from "@/contexts/GameHistoryContext";
 import { useWaitingToPlayContext } from "@/contexts/IsWaitingToPlay";
 
 export const TotalWon = () => {
-  const { totalWon } = useGameHistory();
+  const { totalWon, currentGameHistory } = useGameHistory();
   const { isWaitingToPlay } = useWaitingToPlayContext();
+  // Extract the first earningsValue, if it exists
+  const firstEarningsValue = currentGameHistory?.[0]?.earningsValue
+    ? (Math.round(currentGameHistory[0].earningsValue * 100) / 100).toFixed(2)
+    : "0.00"; // Fallback to "+0.00" if no earningsValue is available
 
   return (
-    <div className="w-[183px] h-[41px] px-5 py-2.5 bg-emerald-400 rounded-full shadow flex justify-center items-center gap-2.5">
+    <div className="w-[205px] h-[41px] px-5 py-2.5 bg-emerald-400 rounded-full shadow flex justify-center items-center gap-2.5">
       <div className="text-black text-opacity-80 text-base font-medium leading-[18.40px] whitespace-nowrap">
         {isWaitingToPlay ? "Loading..." : "Round Earnings"}
       </div>
       {!isWaitingToPlay && (
-        <div className="text-black text-lg font-bold font-['Inter'] leading-tight">
-          {totalWon !== -1
-            ? parseFloat(totalWon.toString()).toFixed(2)
-            : "0.00"}
+        <div className="flex items-baseline text-black text-lg font-bold font-['Inter'] leading-tight">
+          {totalWon !== -1 ? (
+            <>
+              {parseFloat(totalWon.toString()).toFixed(2)}
+              <span className="text-sm font-normal">+{firstEarningsValue}</span>
+            </>
+          ) : (
+            "0.00"
+          )}
         </div>
       )}
     </div>
